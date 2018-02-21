81fc98a8e689ca05f76c54c184f2750ecbc8178b
aishu <aishwarya.kore5696@gmail.com>
2018-02-21 16:44:29 +0530

    [TDD]removed unnecessary testcases and added notes

--- TestDrivenDevelopment/TestDrivenDevelopment/test/substring/CommonSubstringTest.java
+++ TestDrivenDevelopment/TestDrivenDevelopment/test/substring/CommonSubstringTest.java
@@ -19,27 +19,13 @@ import substring.CommonSubstring;
  * @author lenovo
  */
 public class CommonSubstringTest {
-    @Test
-    public void returnZeroWhenBothInputsAreEmpty() {
-       
-        CommonSubstring commonSubstring= new CommonSubstring();
-        int result=commonSubstring.find("","");
-        assertEquals(0,result);
-    }
+  
      @Test
     public void returnZeroWhenOneInputsIsEmpty() {
        
         CommonSubstring commonSubstring= new CommonSubstring();
         int result=commonSubstring.find("","as");
         assertEquals(0,result);
-    }
-     @Test
-    public void returnLengthWhenBothInputsAreEqual() {
-       
-        CommonSubstring commonSubstring= new CommonSubstring();
-        int result=commonSubstring.find("ab","ab");
-       
-        assertEquals(2,result);
     }
      @Test
     public void returnZeroWhenBothStringsUnequal() {
@@ -56,14 +42,6 @@ public class CommonSubstringTest {
         int result=commonSubstring.find("12ab","xz12abghjk");
       // System.out.println("res:"+result);
         assertEquals(4,result);
-    }
-   @Test
-    public void returnLengthWhenBeginningOfSmallerIsSubstringOfLarger() {
-       
-        CommonSubstring commonSubstring= new CommonSubstring();
-        int result=commonSubstring.find("abcdef","hijabcklm");
-        //System.out.println("res:"+result);
-        assertEquals(3,result);
     }
     @Test
     public void returnLengthWhenBothStringsHaveACommonSubstring() {
