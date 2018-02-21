e027982b3e86f49f65bcda7fec0f0f2b7026c90a
Abhinav <abhinav.manchanda@gmail.com>
2018-02-21 17:04:50 +0530

    Added a blank display board.

---
+++ src/main/java/parkingsystem/DisplayBoard.java
@@ -0,0 +1,8 @@
+package parkingsystem;
+
+public class DisplayBoard {
+
+    public void display(int availableParkings){
+        //This method is intentionally left blank, and is present only to help explain the concept.
+    }
+}
