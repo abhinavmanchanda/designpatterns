2f1f1e86a768d06499bac9f83a2e6b18d73f6ae3
aishu <aishwarya.kore5696@gmail.com>
2018-02-21 11:27:00 +0530

    [TDD]test for empty string

---
+++ TestDrivenDevelopment/TestDrivenDevelopment/src/substring/CommonSubstring.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package substring;
+
+/**
+ *
+ * @author lenovo
+ */
+public class CommonSubstring {
+
+    int find(String string, String string0) {
+      return 0;
+    }
+    
+}
---
+++ TestDrivenDevelopment/TestDrivenDevelopment/test/substring/CommonSubstringTest.java
@@ -0,0 +1,37 @@
+package substring;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import substring.CommonSubstring;
+
+/**
+ *
+ * @author lenovo
+ */
+public class CommonSubstringTest {
+    @Test
+    public void returnZeroWhenBothInputsAreEmpty() {
+       
+        CommonSubstring commonSubstring= new CommonSubstring();
+        int result=commonSubstring.find("","");
+        assertEquals(0,result);
+    }
+    
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
