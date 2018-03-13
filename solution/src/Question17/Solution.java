b3b1c7491123a603b93a43423f1d2245fd04d742
aishu <aishwarya.kore5696@gmail.com>
2018-03-13 18:05:38 +0530

    liskov substitution

--- solution/src/Question17/Solution.java
+++ solution/src/Question17/Solution.java
@@ -1,30 +1,23 @@
 
-import adapter.Conference;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-public class Conference1 {
+ class Conference {
 
-    ArrayList<Session1> sessions;
+    ArrayList<Session> sessions;
 
-    Conference1(ArrayList session) {
+    Conference(ArrayList session) {
         this.sessions = session;
     }
 
-  
-}
-class Adapter
-{
-    Map<String, List<String>> convert(Conference1 conference)
-    {
-        Map<String, List<String>> speakerSessionMapping = new HashMap<String, List<String>>();
-         
+    public Map returnlist() {
 
-        for (Session1 session : conference.sessions)
-        {
+        Map<String, List<String>> speakerSessionMapping = new HashMap<String, List<String>>();
+        for (Session session : sessions) {
             String sessionName = session.name;
             String speaker = session.speaker;
             if (!speakerSessionMapping.containsKey(speaker)) {
@@ -35,39 +28,35 @@ class Adapter
         }
         return speakerSessionMapping;
     }
-    
-   
 }
+
 public class Solution {
 
     public static void main(String[] args) {
-        ArrayList<Session1> sessions = new ArrayList<>();
+        ArrayList<Session> sessions = new ArrayList<>();
         Map<String, List<String>> speakerSessionMapping = new HashMap<String, List<String>>();
-        Session1 s1 = new Session1("JS", "AAA", "9am");
-        Session1 s2 = new Session1("AWS", "BBB", "1pm");
-        Session1 s3 = new Session1("JAVA", "AAA", "3pm");
+        Session s1 = new Session("JS", "AAA", "9am");
+        Session s2 = new Session("AWS", "BBB", "1pm");
+        Session s3 = new Session("JAVA", "AAA", "3pm");
 
         sessions.add(s1);
         sessions.add(s2);
         sessions.add(s3);
-        
-        Conference1 conference = new Conference1(sessions);
-               
-        Adapter adapter=new Adapter();
-        speakerSessionMapping=adapter.convert(conference);
+        Conference conference = new Conference(sessions);
+        speakerSessionMapping=conference.returnlist();
          for(Map.Entry m:speakerSessionMapping.entrySet()){  
                 System.out.println(m.getKey()+" "+m.getValue());  
   }  
     }
 }
 
-class Session1 {
+class Session {
 
     String name;
     String time;
     String speaker;
 
-    Session1(String name, String speaker, String time) {
+    Session(String name, String speaker, String time) {
         this.name = name;
         this.speaker = speaker;
         this.time = time;
